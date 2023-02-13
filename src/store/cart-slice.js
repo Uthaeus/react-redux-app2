c52220952cafc5c29761733970f2517ab37fc179
Uthaeus <romanlavery@gmail.com>
2023-02-13 12:23:55 -0700

    fixed fetch initial load and remove item from cart bugs

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -5,7 +5,8 @@ const cartSlice = createSlice({
     name: 'cart',
     initialState: {
         items: [],
-        totalQuantity: 0
+        totalQuantity: 0,
+        changed: false
     },
     reducers: {
         replaceCart(state, action) {
@@ -16,6 +17,7 @@ const cartSlice = createSlice({
             const newItem = action.payload;
             const existingItem = state.items.find(item => item.id === newItem.id);
             state.totalQuantity++;
+            state.changed = true;
 
             if (!existingItem) {
                 state.items.push({
@@ -34,6 +36,7 @@ const cartSlice = createSlice({
             const id = action.payload;
             const existingItem = state.items.find(item => item.id === id);
             state.totalQuantity--;
+            state.changed = true;
 
             if (existingItem.quantity === 1) {
                 state.items = state.items.filter(item => item.id !== id);
